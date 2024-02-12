90f04003dc3a2d1c5c066d57b0bf8a83c294955c
Daniel Tranca <danieltrancax@gmail.com>
2024-02-12 22:58:30 +0100

    chore: upgraded FKU and project

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -6,23 +6,22 @@ namespace FlowEnt.Demo
 {
     public class SceneManager : MonoBehaviour
     {
-#pragma warning disable IDE0051, RCS1213
         private void Awake()
         {
 #if UNITY_EDITOR
             const string url = "https://demo.flowent.friedsynapse.com?scdene=character";
 #else
-        string url = Application.absoluteURL;
+            string url = Application.absoluteURL;
 #endif
-            Uri myUri = new Uri(url);
+            Uri myUri = new(url);
             string sceneParam = HttpUtility.ParseQueryString(myUri.Query).Get("scene");
             string sceneName = sceneParam switch
             {
                 "character" => "FlowEnt.Demo.Character",
+                "authoring" => "FlowEnt.Demo.Authoring",
                 _ => "FlowEnt.Demo",
             };
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
         }
-#pragma warning restore IDE0051, RCS1213
     }
 }
